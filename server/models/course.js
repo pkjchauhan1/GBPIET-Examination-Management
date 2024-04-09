1eed7602b82bf569aea0ffc135b57e549b3362c0
Pankaj <pkjchauhan1@gmail.com>
2024-04-09 17:48:24 +0530

    pankaj

--- server/models/course.js
+++ server/models/course.js
@@ -1,9 +1,13 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const courseSchema = mongoose.Schema({
   course: {
     type: String,
     required: true,
+  },
+  faculty_id: {
+    type: Schema.Types.ObjectId,
+    ref: "faculty",
   },
 });
 
