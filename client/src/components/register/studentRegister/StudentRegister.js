0cd6202045ad5cfd30c622069ed6ca560bde3839
Ajay <ajayprakashjoshi1@gmail.com>
2024-04-16 15:16:53 +0530

    Ajay

--- client/src/components/register/studentRegister/StudentRegister.js
+++ client/src/components/register/studentRegister/StudentRegister.js
@@ -7,17 +7,18 @@ import Select from "react-select";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm, Controller } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import Modal from "react-modal";
 
 const schema = yup
   .object({
     name: yup.string().required(),
     gender: yup.string().required(),
-    course: yup.string().required(),
+    course: yup.array().required(),
     year: yup.string().required(),
     semester: yup.string().required(),
     university_roll_no: yup.string().required(),
     university_enrollment_no: yup.string().required(),
-    gender: yup.string(),
     father_name: yup.string(),
     contact_number: yup.string().required(),
     email: yup.string().email().required(),
@@ -39,6 +40,8 @@ const defaultValues = {
   batch: "",
 };
 
+Modal.setAppElement("#root");
+
 const StudentRegister = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -46,22 +49,22 @@ const StudentRegister = () => {
   const [semester, setSemester] = useState("");
   const [error, setError] = useState({});
   const [loading, setLoading] = useState(false);
-  const [gender, setGender] = useState(undefined);
+  const [gender, setGender] = useState("");
   const [course, selectCourse] = useState([]);
   const [father_name, setFatherName] = useState("");
-  const [subjects, setSubjects] = useState(undefined);
   const [contact_number, setContactNumber] = useState("");
   const [university_roll_no, setUniversityRollNo] = useState("");
   const [university_enrollment_no, setUniversityEnrollmentNo] = useState("");
   const [translate, setTranslate] = useState(false);
   const [courses, setCourses] = useState([]);
   const [batch, setBatch] = useState("");
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const {
     control,
-    setValue,
     handleSubmit,
     formState: { errors },
   } = useForm({
@@ -103,11 +106,13 @@ const StudentRegister = () => {
       batch,
     } = data;
 
+    const courseIds = course.map((c) => c.value);
+
     dispatch(
       addStudent({
         name,
         email,
-        course,
+        course: courseIds,
         contact_number,
         year,
         semester,
@@ -117,28 +122,23 @@ const StudentRegister = () => {
         gender,
         batch,
       })
-    );
-  };
+    )
+    .then(() => {
+      setModalIsOpen(true);
+    })
+    .catch((error) => {
+      console.error("Registration failed", error);
+    });
+};
 
   return (
+    <>
     <div className="bg-[#04bd7d] h-screen w-screen flex items-center justify-center">
-      <div className="grid grid-cols-2">
-        <div
-          className={`h-[40rem] w-full bg-white flex items-center justify-center ${
-            translate ? "translate-x-[24rem]" : ""
-          }  duration-1000 transition-all rounded-3xl shadow-2xl`}
-        >
-          <h1 className="text-[3rem]  font-bold text-center">
-            Student
-            <br />
-            Register
-          </h1>
-        </div>
+      <div className="flex">
+ 
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className={`h-[40rem] w-full bg-[#2c2f35] grid grid-cols-3 gap-4 p-[2rem] ${
-            translate ? "-translate-x-[24rem]" : ""
-          }  duration-1000 transition-all rounded-3xl shadow-2xl`}
+          className={`h-[40rem] w-full bg-[#2c2f35] grid gap-4 p-[2rem] rounded-3xl shadow-2xl`}
         >
           <h1 className="text-white text-3xl font-semibold col-span-3">
             Student
@@ -150,7 +150,7 @@ const StudentRegister = () => {
                 required
                 type="text"
                 value={name}
-                placeholder="John Doe"
+                placeholder="Enter your name"
                 onChange={(e) => setName(e.target.value)}
                 className="bg-[#515966] text-white px-2 outline-none py-2 rounded-lg placeholder:text-sm"
               />
@@ -163,65 +163,39 @@ const StudentRegister = () => {
                 required
                 type="email"
                 value={email}
-                placeholder="johndoe@email.com"
+                placeholder="Enter Your Email"
                 onChange={(e) => setEmail(e.target.value)}
                 className="bg-[#515966] text-white px-2 outline-none py-2 rounded-lg placeholder:text-sm"
               />
             </div>
           </div>
-          <div className="space-y-1">
+
+<div className="space-y-1">
             <p className="text-[#515966] font-bold text-sm">Gender</p>
-            <div className="bg-[#515966] rounded-lg w-[14rem] flex  items-center">
+            <div className="bg-[#515966] rounded-lg w-[14rem] flex items-center">
               <select
-                name="gender"
+                required
+                className="bg-[#515966] text-white px-2 outline-none py-2 rounded-lg w-full"
                 value={gender}
                 onChange={(e) => setGender(e.target.value)}
-                className="w-[13.5rem] bg-[#515966] text-white px-2 outline-none py-2 rounded-lg placeholder:text-sm"
               >
-                <option value="male">Male</option>
-                <option value="female">Female</option>
+              <option value="" disabled>
+                        Select
+                      </option>
+                      <option value="male">Male</option>
+                      <option value="female">Female</option>
               </select>
             </div>
           </div>
 
           <div className="space-y-1">
-            <p className="text-[#515966] font-bold text-sm">Course</p>
-            <div
-              className={`bg-[#515966] rounded-lg w-[14rem] ${
-                errors.course ? "border border-red-500" : ""
-              }`}
-            >
-              <Controller
-                name="course"
-                control={control}
-                rules={{ required: true }}
-                render={({ field }) => (
-                  <Select
-                    {...field}
-                    options={courses.map((course) => ({
-                      value: course.course,
-                      label: course.course,
-                    }))}
-                    isMulti
-                    className="text-black placeholder:text-sm"
-                    classNamePrefix="select"
-                    theme={(theme) => ({
-                      ...theme,
-                      borderRadius: 5,
-                      colors: {
-                        ...theme.colors,
-                        primary25: "grey",
-                        primary: "white",
-                      },
-                    })}
-                    onChange={(selectedOptions) => {
-                      setValue(
-                        "course",
-                        selectedOptions.map((option) => option.value)
-                      );
-                    }}
-                  />
-                )}
+            <p className="text-[#515966] font-bold text-sm">Contact Number</p>
+            <div className="bg-[#515966] rounded-lg w-[14rem] flex  items-center">
+              <input
+                value={contact_number}
+                placeholder="10 Digit Number"
+                onChange={(e) => setContactNumber(e.target.value)}
+                className="bg-[#515966] text-white px-2 outline-none py-2 rounded-lg placeholder:text-sm"
               />
             </div>
           </div>
@@ -232,7 +206,7 @@ const StudentRegister = () => {
               <input
                 type="text"
                 value={father_name}
-                placeholder="John Doe"
+                placeholder="Father Name"
                 onChange={(e) => setFatherName(e.target.value)}
                 className="bg-[#515966] text-white px-2 outline-none py-2 rounded-lg placeholder:text-sm"
               />
@@ -240,18 +214,40 @@ const StudentRegister = () => {
           </div>
 
           <div className="space-y-1">
-            <p className="text-[#515966] font-bold text-sm">Contact Number</p>
-            <div className="bg-[#515966] rounded-lg w-[14rem] flex  items-center">
-              <input
-                required
-                type="number"
-                value={contact_number}
-                placeholder="10 Digit Number"
-                onChange={(e) => setContactNumber(e.target.value)}
-                className="bg-[#515966] text-white px-2 outline-none py-2 rounded-lg placeholder:text-sm"
-              />
+              <p className="text-[#515966] font-bold text-sm">Course</p>
+              <div
+                className={`bg-[#515966] rounded-lg w-[14rem] ${
+                  errors.course ? "border border-red-500" : ""
+                }`}
+              >
+                <Controller
+                  name="course"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <Select
+                      {...field}
+                      options={courses.map((course) => ({
+                        value: course._id,
+                        label: course.course,
+                      }))}
+              
+                      className="text-black placeholder:text-sm"
+                      classNamePrefix="select"
+                      theme={(theme) => ({
+                        ...theme,
+                        borderRadius: 5,
+                        colors: {
+                          ...theme.colors,
+                          primary25: "grey",
+                          primary: "white",
+                        },
+                      })}
+                    />
+                  )}
+                />
+              </div>
             </div>
-          </div>
 
           <div className="space-y-1">
             <p className="text-[#515966] font-bold text-sm">Year</p>
@@ -263,9 +259,8 @@ const StudentRegister = () => {
                 onChange={(e) => setYear(e.target.value)}
               >
                 <option value="" disabled>
-                  Select year
+                  Select Year
                 </option>
-                <option value="none">none</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -284,9 +279,8 @@ const StudentRegister = () => {
                 onChange={(e) => setSemester(e.target.value)}
               >
                 <option value="" disabled>
-                  Select semester
+                  Select Semester
                 </option>
-                <option value="none">none</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -300,13 +294,23 @@ const StudentRegister = () => {
           </div>
 
           <div className="space-y-1">
+            <p className="text-[#515966] font-bold text-sm">Batch</p>
+            <div className="bg-[#515966] rounded-lg w-[14rem] flex  items-center">
+              <input
+                value={batch}
+                placeholder="Enter Your Batch"
+                onChange={(e) => setBatch(e.target.value)}
+                className="bg-[#515966] text-white px-2 outline-none py-2 rounded-lg placeholder:text-sm"
+              />
+            </div>
+          </div>
+
+          <div className="space-y-1">
             <p className="text-[#515966] font-bold text-sm">
               University Roll No
             </p>
             <div className="bg-[#515966] rounded-lg w-[14rem] flex  items-center">
               <input
-                required
-                type="number"
                 value={university_roll_no}
                 placeholder="12 Digit Number"
                 onChange={(e) => setUniversityRollNo(e.target.value)}
@@ -320,8 +324,6 @@ const StudentRegister = () => {
             </p>
             <div className="bg-[#515966] rounded-lg w-[14rem] flex  items-center">
               <input
-                required
-                type="number"
                 value={university_enrollment_no}
                 placeholder="12 Digit Number"
                 onChange={(e) => setUniversityEnrollmentNo(e.target.value)}
@@ -352,6 +354,50 @@ const StudentRegister = () => {
         </form>
       </div>
     </div>
+    <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        contentLabel="Registration Successful"
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0, 0, 0, 0.75)",
+          },
+          content: {
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+            border: "none",
+            background: "#2c2f35",
+            borderRadius: "10px",
+            padding: "20px",
+            color: "white",
+            width: "30rem",
+            boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+          },
+        }}
+      >
+        <h2 className="text-xl font-bold text-center mb-4">
+          Registration Successful!
+        </h2>
+        <p className="text-center">
+          Please check your email for your login ID and password.
+        </p>
+        <div className="mt-4 flex justify-center">
+          <button
+            onClick={() => {
+              setModalIsOpen(false);
+              navigate("/");
+            }}
+            className="bg-[#04bd7d] text-white font-medium py-2 px-6 rounded hover:bg-green-600 transition duration-150"
+          >
+            OK
+          </button>
+        </div>
+      </Modal>
+    </>
   );
 };
 
