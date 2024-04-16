21142da07cc0421dbff4abacb8e17021f0b9b34f
Pankaj <pkjchauhan1@gmail.com>
2024-04-16 14:32:13 +0530

    Pankaj

--- client/src/components/login/facultyLogin/FacultyLogin.js
+++ client/src/components/login/facultyLogin/FacultyLogin.js
@@ -51,126 +51,128 @@ const FacultyLogin = () => {
   };
 
   return (
-    <div className="bg-[#04bd7d] h-screen w-screen flex items-center justify-center">
-     
-      <div className="grid grid-cols-2">
-        <div
-          className={`h-96 w-96 bg-white flex items-center justify-center ${
-            translate ? "translate-x-[12rem]" : ""
-          }  duration-1000 transition-all rounded-3xl shadow-2xl`}
-        >
-          <h1 className="text-[3rem]  font-bold text-center">
-            Faculty
-            <br />
-            Login
-          </h1>
-        </div>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className={`${
-            loading ? "h-[27rem]" : "h-96"
-          } w-96 bg-[#2c2f35] flex flex-col items-center justify-center ${
-            translate ? "-translate-x-[12rem]" : ""
-          }  duration-1000 transition-all space-y-6 rounded-3xl shadow-2xl`}
-        >
-          <h1 className="text-white text-3xl font-semibold">Faculty</h1>
-          <div className="space-y-1">
-            <p className="text-[#515966] font-bold text-sm">Email</p>
-            <div
-              className={`bg-[#515966] rounded-lg w-[14rem] flex  items-center ${
-                errors.email ? "border border-red-500" : ""
-              }`}
-            >
-              <Controller
-                name="email"
-                control={control}
-                rules={{ required: true }}
-                render={({ field }) => (
-                  <input
-                    type="text"
-                    placeholder="Email"
-                    className="bg-[#515966] text-white px-2 outline-none py-2 rounded-lg placeholder:text-sm"
-                    {...field}
-                  />
-                )}
-              />
-            </div>
+    <>
+      <div className="bg-[#04bd7d] h-screen w-screen flex items-center justify-center">
+        <div className="flex items-center justify-center">
+          <div
+            className={`h-96 w-96 bg-white flex items-center justify-center ${
+              translate ? "translate-x-[12rem]" : ""
+            }  duration-1000 transition-all rounded-3xl shadow-2xl`}
+          >
+            <h1 className="text-[3rem] font-bold text-center">
+              Faculty
+              <br />
+              Login
+            </h1>
           </div>
-          <div className="space-y-1">
-            <p className="text-[#515966] font-bold text-sm">Password</p>
-            <div
-              className={`bg-[#515966] rounded-lg px-2 flex items-center ${
-                errors.password ? "border border-red-500" : ""
-              }`}
-            >
-              <Controller
-                name="password"
-                control={control}
-                rules={{ required: true }}
-                render={({ field }) => (
-                  <>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className={`${
+              loading ? "h-[27rem]" : "h-96"
+            } w-96 bg-[#2c2f35] flex flex-col items-center justify-center ${
+              translate ? "-translate-x-[12rem]" : ""
+            }  duration-1000 transition-all space-y-6 rounded-3xl shadow-2xl`}
+          >
+            <h1 className="text-white text-3xl font-semibold">Faculty</h1>
+            <div className="space-y-1">
+              <p className="text-[#515966] font-bold text-sm">Login ID</p>
+              <div
+                className={`bg-[#515966] rounded-lg w-[14rem] flex  items-center ${
+                  errors.email ? "border border-red-500" : ""
+                }`}
+              >
+                <Controller
+                  name="email"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
                     <input
-                      placeholder="Password"
-                      type={showPassword ? "text" : "password"}
-                      pattern="^(?=.*[A-Z])(?=.*[@])(?=.*\d).{6,}$"
-                      title="USE ONE : @-Number-UpperCase (at least 6 character)"
-                      className="bg-[#515966] text-white rounded-lg outline-none py-2  placeholder:text-sm"
+                      type="text"
+                      placeholder="Enter Your Login ID"
+                      className="bg-[#515966] text-white px-2 outline-none py-2 rounded-lg placeholder:text-sm"
                       {...field}
                     />
-                    {showPassword ? (
-                      <VisibilityIcon
-                        onClick={() => setShowPassword(!showPassword)}
-                        className="cursor-pointer"
-                      />
-                    ) : (
-                      <VisibilityOffIcon
-                        onClick={() => setShowPassword(!showPassword)}
-                        className="cursor-pointer"
-                      />
-                    )}
-                  </>
-                )}
-              />
+                  )}
+                />
+              </div>
             </div>
-          </div>
-          <button
-            type="submit"
-            className="w-32 hover:scale-105 transition-all duration-150 rounded-lg flex items-center justify-center text-white text-base py-1 bg-[#04bd7d]"
-          >
-            Login
-          </button>
-          <a href="/" className="w-32 hover:scale-105 transition-all duration-150 rounded-lg flex items-right justify-center text-white text-base py-1 bg-[#FF2400]">
-          Home
-      </a>
+            <div className="space-y-1">
+              <p className="text-[#515966] font-bold text-sm">Password</p>
+              <div
+                className={`bg-[#515966] rounded-lg px-2 flex items-center ${
+                  errors.password ? "border border-red-500" : ""
+                }`}
+              >
+                <Controller
+                  name="password"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <>
+                      <input
+                        placeholder="Password"
+                        type={showPassword ? "text" : "password"}
+                        pattern="^(?=.*[A-Z])(?=.*[@])(?=.*\d).{6,}$"
+                        title="USE ONE : @-Number-UpperCase (at least 6 character)"
+                        className="bg-[#515966] text-white rounded-lg outline-none py-2  placeholder:text-sm"
+                        {...field}
+                      />
+                      {showPassword ? (
+                        <VisibilityIcon
+                          onClick={() => setShowPassword(!showPassword)}
+                          className="cursor-pointer"
+                        />
+                      ) : (
+                        <VisibilityOffIcon
+                          onClick={() => setShowPassword(!showPassword)}
+                          className="cursor-pointer"
+                        />
+                      )}
+                    </>
+                  )}
+                />
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="w-32 hover:scale-105 transition-all duration-150 rounded-lg flex items-center justify-center text-white text-base py-1 bg-[#04bd7d]"
+            >
+              Login
+            </button>
+            <a
+              href="/"
+              className="w-32 hover:scale-105 transition-all duration-150 rounded-lg flex items-right justify-center text-white text-base py-1 bg-[#FF2400]"
+            >
+              Home
+            </a>
 
-          {loading && (
-            <Spinner
-              message="Logging In"
-              height={30}
-              width={150}
-              color="#ffffff"
-              messageColor="#fff"
-            />
-          )}
-          <ul className="mt-2">
-            {errors.email ? (
-              <li>
-                <small className="text-red-500">
-                  {errors.email.message}
-                </small>
-              </li>
-            ) : null}
-            {errors.password ? (
-              <li>
-                <small className="text-red-500">
-                  {errors.password.message}
-                </small>
-              </li>
-            ) : null}
-          </ul>
-        </form>
+            {loading && (
+              <Spinner
+                message="Logging In"
+                height={30}
+                width={150}
+                color="#ffffff"
+                messageColor="#fff"
+              />
+            )}
+            <ul className="mt-2">
+              {errors.email ? (
+                <li>
+                  <small className="text-red-500">{errors.email.message}</small>
+                </li>
+              ) : null}
+              {errors.password ? (
+                <li>
+                  <small className="text-red-500">
+                    {errors.password.message}
+                  </small>
+                </li>
+              ) : null}
+            </ul>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
